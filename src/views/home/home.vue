f20aa59830b5c0bcb704da069b03b2fed5fc864b
橡皮擦 <2506152074@qq.com>
2023-01-09 17:18:20 +0800

    主页功能开发

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -12,12 +12,23 @@
     <div class="middle">
       <!-- 轮播图 -->
       <div class="carousel">
-        <el-carousel indicator-position="none" arrow="never" height="520px">
+        <el-carousel ref="carouselRef" arrow="never" height="520px" :autoplay="false">
           <el-carousel-item v-for="item in list" :key="item">
             <div>
               <div class="carousel-img-box">
                 <img class="carousel-img" :src="item.src" />
-                <div class="carousel-bottom"></div>
+                <div class="carousel-bottom">
+                  <div class="carousel-title">绝美非遗绒花，超强梦幻联动！</div>
+                  <div class="toggle-button">
+                    <div class="button-item">
+                      <SvgIcon name="rightArrow" class="arrow-icon rotation" color="#fff" @click="carouselSwitch(false)">
+                      </SvgIcon>
+                    </div>
+                    <div class="button-item">
+                      <SvgIcon name="leftArrow" class="arrow-icon" color="#fff" @click="carouselSwitch(true)"></SvgIcon>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </el-carousel-item>
@@ -35,17 +46,32 @@
 import Card from "@/components/homeVideoList/card.vue"
 import homeHeaderChannel from "@/components/homeHeaderChannel/homeHeaderChannel.vue"
 import topNavigation from "@/components/topNavigation/topNavigation.vue"
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 
 components: {
   Card
   homeHeaderChannel
   topNavigation
 }
+
+const carouselRef = ref()
 const list = reactive([
   // { src: "https://eraser-go-live.oss-cn-hangzhou.aliyuncs.com/assets/static/img/articleContribution/e41fd63193dbde45f822733e148d30facc1d05dc14dc163a20fe72bdf6fc8c2a.jpg" },
   { src: "/src/assets/img/temporary/adasdasd.png" },
+  { src: "/src/assets/img/temporary/adasdasd.png" },
+  { src: "/src/assets/img/temporary/adasdasd.png" },
 ])
+
+//切换轮播图 true 下一张 false 上一张
+const carouselSwitch = (is: boolean) => {
+  if (is) {
+    carouselRef.value.next()
+
+  } else {
+    carouselRef.value.prev()
+  }
+
+}
 </script>
 
 <style scoped lang="scss">
