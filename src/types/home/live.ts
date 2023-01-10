46dfbf219a64044eb03e8808598e031cadde15f3
橡皮擦 <2506152074@qq.com>
2023-01-10 22:51:59 +0800

    添加类型

--- src/types/home/live.ts
+++ src/types/home/live.ts
@@ -1,5 +1,20 @@
+import { type } from "os"
+
 //getLiveRoom 获取直播接口请求需要
-export interface getLiveRoomRes  {
-    address :string,
-    key : string
+export interface getLiveRoomRes {
+    address: string,
+    key: string
+}
+  
+//轮播图
+export interface rotograph {
+    title: string
+    cover: string
+    color: string
+    type: string
+    to_id: number
+}
+export type rotographList = Array<rotograph>
+export interface getHomeInfoRes {
+    rotograph: rotographList
 }
