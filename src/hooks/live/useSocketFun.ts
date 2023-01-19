e682b4f1642d2a766f56bcabcd5bc29ba85333b5
橡皮擦 <2506152074@qq.com>
2023-01-20 01:26:01 +0800

    直播socket调整

--- src/hooks/live/useSocketFun.ts
+++ src/hooks/live/useSocketFun.ts
@@ -1,6 +1,22 @@
 import { Message, decodeWebClientSendBarrageRes ,decodeWebClientEnterLiveRoomRes ,decodeWebClientHistoricalBarrageRes} from "@/proto/pb/live"
 import DPlayer, { DPlayerDanmakuItem } from "dplayer"
+import globalScss from "@/assets/styles/global/export.module.scss"
+import Swal from "sweetalert2";
 import { Ref } from "vue";
+import { Uint8ArrayToString } from "@/utils/conversion/unt8Array";
+ import router from "@/router";
+
+export const webError = (data: Message) => {
+    Swal.fire({
+        title: new TextDecoder("utf-8").decode(data.data as Uint8Array),
+        heightAuto: false,
+        confirmButtonColor: globalScss.colorButtonTheme,
+        icon: "error",
+    })
+    router.back()
+    return
+}
+
 export const webClientBarrageDeal = (data: Message, dp: DPlayer, sideRef: Ref<any>) => {
     //格式化消息
     if (!data.data) return;
