fa28c051afee887f624e7b4133db774e1a2e8f89
橡皮擦 <2506152074@qq.com>
2023-01-20 01:22:53 +0800

    主页视频组件调整

--- src/components/homeVideoList/card.vue
+++ src/components/homeVideoList/card.vue
@@ -3,31 +3,31 @@
     <div :class="{ item: true }" @click="jump()">
         <div :class="{ normal: true, mouseleave: !isMouseover }" v-show="!isMouseover">
             <div class="head">
-                <div class="item-image"></div>
+                <div class="item-image" :style="{backgroundImage : `url(${props.cover})` }"></div>
                 <div class="classification">
                     <div class="classification-left">
                         <span class="play-volume">
                             <SvgIcon name="video" class="icon" color="#fff"></SvgIcon>
-                            <span>21.8万</span>
+                            <span>{{ props.heat }}</span>
                         </span>
                         <span class="barrage-number">
                             <SvgIcon name="barrageNumber" class="icon" color="#fff"></SvgIcon>
-                            <span>342</span>
+                            <span>{{ props.barrageNumber }}</span>
                         </span>
                     </div>
                     <div class="classification-right">
-                        <span>11:21</span>
+                        <span>{{ formattingSecondTime(props.video_duration) }}</span>
                     </div>
 
 
                 </div>
             </div>
             <div class="info">
-                <div class="video-title">普通男人爱情最真实的样子…………就是这样了</div>
+                <div class="video-title">{{ props.title }}</div>
                 <div class="video-information">
                     <SvgIcon name="up" class="icon" color="#999"></SvgIcon>
-                    <span class="video-information-username">天师道的白山正 </span> <span class="video-information-time">·
-                        1-5</span>
+                    <span class="video-information-username">{{ props.username }} </span> <span class="video-information-time">·
+                        {{ timestampFormat(props.created_at) }}</span>
                 </div>
             </div>
         </div>
@@ -63,14 +63,43 @@
 <script setup lang="ts">
 
 import { ref, defineProps } from "vue"
+import {formattingSecondTime , timestampFormat ,timetoRFC3339 ,rFC3339ToTime} from "@/utils/conversion/timeConversion"
 import { useRouter } from "vue-router"
 
 
-const emit = defineProps({
-    roomID: {
+const props = defineProps({
+    id: {
         type: Number,
         required: true,
-    }
+    },
+    video_duration: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    cover : {
+        type: String,
+        required: true,
+    },
+    heat: {
+        type: Number,
+        required: true,
+    },
+    barrageNumber: {
+        type: Number,
+        required: true,
+    },
+    username : {
+        type: String,
+        required: true,
+    },
+    created_at : {
+        type: String,
+        required: true,
+    },
 }
 )
 
@@ -87,7 +116,7 @@ const isMouseover = ref(false)
 // }
 
 const jump = () => {
-    router.push({ name: "liveRoom", query: { roomID: emit.roomID } })
+    router.push({ name: "VideoShow", query: { roomID: props.id } })
 }
 
 </script>
