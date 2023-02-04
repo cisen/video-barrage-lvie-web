a8d6565746e956208b8370b9787b2816fa94bc0d
橡皮擦 <2506152074@qq.com>
2023-02-04 23:53:28 +0800

    直播我的专栏调整

--- src/components/LiveBroadcast/column.vue
+++ src/components/LiveBroadcast/column.vue
@@ -4,10 +4,10 @@
         <div class="column-list">
             <!-- 单个卡片 -->
             <div :class="{ mouseover: item.is_stay, mouseleave: !item.is_stay }" class="column-item shadow-box "
-                @mouseover="mouseOver(item)" @mouseleave="mouseleave(item)" v-for="item in columnList" :key="item.id"
+                @mouseover="mouseOver(item)" @mouseleave="mouseleave(item)" v-for="(item,index) in columnList" :key="item.id"
                 @click="jumpArticle(item.id)">
-                <div class="item-image">
-                    <img :src="item.cover" class="el-image__inner" style="object-fit: cover;">
+                <div :class="{'item-image': true ,'right' : index%2 == 1}">
+                    <img :src="item.cover" class="el-image__inner image" style="object-fit: cover;">
                 </div>
                 <div class="item-text">
                     <div class="post-meta">
@@ -30,7 +30,13 @@
                         </span>
                     </div>
                     <div class="recent-post-desc">
-                        {{ item.content }} </div>
+
+                        <VueEllipsis3 :text="item.content" :visibleLine="4">
+                            <template v-slot:ellipsisNode>
+                                <span>...</span>
+                            </template>
+                        </VueEllipsis3>
+                      </div>
                     <div class="sort-label">
                         <span class="lable-item" style="margin-right: 12px;" v-for="label in item.label" :key="label">
                             <SvgIcon name="label" class="icon-small" /> {{ label }}
@@ -51,6 +57,12 @@ import { GetArticleContributionListByUserRes, GetArticleContributionListByUserRe
 import { onMounted, reactive, ref } from 'vue';
 import dayjs from "dayjs"
 import { useRouter } from 'vue-router';
+import { VueEllipsis3 } from 'vue-ellipsis-3';
+
+components: {
+    VueEllipsis3
+}
+
 
 const props = defineProps({
     roomID: {
